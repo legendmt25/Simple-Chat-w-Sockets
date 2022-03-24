84980f95d1285e0faacfba7cf55c7f558420ab4e
Мартин Трајковски <martintrajkovski70@gmail.com>
2022-03-25 00:36:45 +0100

    vue client added

---
+++ client/src/views/ChatView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { WebSocket,  } from "ws";
+export default {
+  name: "chatView",
+  data() {
+    return {
+      socket: null,
+    };
+  },
+  created() {
+    console.log(new WebSocket("ws://localhost:4000"));
+  },
+};
+</script>
